812fb11be913847d0e2918c5f2e6d8af2e2694f6
Erik Rimskog <erik.rimskog@gmail.com>
2019-04-24 21:54:03 +0200

    lines kinda

--- pages/editor.jsx
+++ pages/editor.jsx
@@ -1,40 +1,172 @@
-const testTree = [
-    {text: "0", color: "orange"},
-    {text: "1", color: "pink"},
-    [
-        [{text: "2", color: "purple"}],
-        [{text: "3", color: "pink"}],
-        [
-            {text: "4", color: "red"},
-            [[{text: "5", color: "red"}], [{text: "6", color: "lime"}]]
-        ]
-    ],
-    {text: "7", color: "green"}
-];
+import {SteppedLineTo} from "react-lineto";
+
+let global_id = 0;
+
+class Node {
+    constructor(text, color = "pink") {
+        this.id = global_id;
+        global_id++;
+        this.text = text;
+        this.color = color;
+    }
+    getHeight() {
+        return 1;
+    }
+}
+
+class Choice {
+    constructor() {
+        this.id = global_id;
+        global_id++;
+        this.list = [];
+    }
+    addBranch(sequence) {
+        this.list.push(sequence);
+        return this;
+    }
+    getEntering() {
+        return this.list.map(x => x.getFirst());
+    }
+    getLeaving() {
+        return Array.prototype.concat(...this.list.map(x => x.getEnds()));
+    }
+    getHeight() {
+        return Math.max(...this.list.map(x => x.getHeight()));
+    }
+}
+
+class Sequence {
+    constructor() {
+        this.id = global_id;
+        global_id++;
+        this.list = [];
+    }
+    addNode(node) {
+        this.list.push(node);
+        return this;
+    }
+    // get the first Node in this sequence
+    getFirst() {
+        const f = this.list[0];
+        if (!f instanceof Node) {
+            throw new Error("getFirst did not return a Node");
+        }
+        return f;
+    }
+    getEnds() {
+        const l = this.list[this.list.length - 1];
+        if (l instanceof Choice) {
+            return l.getLeaving();
+        }
+        return [l]; // must be a Node
+    }
+    getHeight() {
+        return this.list.map(x => x.getHeight()).reduce((a, b) => a + b, 0);
+    }
+}
+
+const testTree = new Sequence()
+    .addNode(new Node("0"))
+    .addNode(new Node("1"))
+    .addNode(
+        new Choice()
+            .addBranch(new Sequence().addNode(new Node("2")))
+            .addBranch(new Sequence().addNode(new Node("3")))
+            .addBranch(
+                new Sequence()
+                    .addNode(new Node("4"))
+                    .addNode(
+                        new Choice()
+                            .addBranch(new Sequence().addNode(new Node("5")))
+                            .addBranch(new Sequence().addNode(new Node("6")))
+                    )
+            )
+    )
+    .addNode(new Node("7"));
+
+class HoriList extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.choice.list.map(x => (
+                    <List sequ={x} />
+                ))}
+                <style jsx>{`
+                    div {
+                        display: flex;
+                        flex-direction: row;
+                    }
+                `}</style>
+            </div>
+        );
+    }
+}
 
 class List extends React.Component {
     constructor(props) {
         super(props);
-        this.horizontal =
-            "horizontal" in this.props ? this.props.horizontal : false;
-    }
-    renderChild(c, i) {
-        if (Array.isArray(c)) {
-            return <List key={i} horizontal={!this.horizontal} list={c} />;
-        } else {
-            return <Square key={i} info={c} />;
-        }
     }
     render() {
         const hori = this.horizontal;
+        const lines = [];
+        const children = [];
+        let last = null;
+
+        for (const x of this.props.sequ.list) {
+            if (x instanceof Choice) {
+                if (last) {
+                    for (const e of x.getEntering()) {
+                        lines.push(
+                            <SteppedLineTo
+                                from={`square-${last.id}`}
+                                to={`square-${e.id}`}
+                            />
+                        );
+                    }
+                }
+                children.push(<HoriList choice={x} />);
+            } else {
+                if (last) {
+                    if (last instanceof Choice) {
+                        for (const e of last.getLeaving()) {
+                            lines.push(
+                                <SteppedLineTo
+                                    from={`square-${x.id}`}
+                                    to={`square-${e.id}`}
+                                />
+                            );
+                        }
+                    } else {
+                        lines.push(
+                            <SteppedLineTo
+                                from={`square-${last.id}`}
+                                to={`square-${x.id}`}
+                            />
+                        );
+                    }
+                }
+                children.push(<Square key={x.id} info={x} />);
+            }
+            last = x;
+        }
+
+        // TODO: react-lineto doesn't work on server rendered applications.
+        //       process.browser is a workaround, create own component
+        //       that uses componentDidMount or something
         return (
-            <div>
-                {this.props.list.map(this.renderChild.bind(this))}
+            <div className="list-container">
+                {children}
+                {process.browser ? lines : null}
+                <div className="anchor" />
                 <style jsx>
                     {`
-                        div {
+                        .list-container {
                             display: flex;
-                            flex-direction: ${hori ? "row" : "column"};
+                            flex-direction: column;
+                        }
+                        .anchor {
+                            margin-top: auto;
+                            width: 100%;
                         }
                     `}
                 </style>
@@ -46,7 +178,7 @@ class List extends React.Component {
 class Square extends React.Component {
     render() {
         return (
-            <div>
+            <div className={`square-${this.props.info.id}`}>
                 {this.props.info.text}
                 <style jsx>{`
                     div {
@@ -62,7 +194,7 @@ class Square extends React.Component {
 }
 
 function Page(props) {
-    return <List list={testTree} />;
+    return <List sequ={testTree} />;
 }
 
 export default Page;
