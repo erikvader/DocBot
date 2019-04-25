f3cbc8b385889fa8a2ccb0925dd57589f3712b65
Erik Rimskog <erik.rimskog@gmail.com>
2019-04-26 00:12:22 +0200

    lines with konva instead

--- pages/editor.jsx
+++ pages/editor.jsx
@@ -1,4 +1,4 @@
-import {SteppedLineTo} from "react-lineto";
+import {Layer, Stage, Line} from "react-konva";
 
 let global_id = 0;
 
@@ -6,7 +6,7 @@ class Node {
     constructor(text, color = "pink") {
         this.id = global_id;
         global_id++;
-        this.text = text;
+        this.text = this.id;
         this.color = color;
     }
     getHeight() {
@@ -32,6 +32,9 @@ class Choice {
     }
     getHeight() {
         return Math.max(...this.list.map(x => x.getHeight()));
+    }
+    getLines() {
+        return Array.prototype.concat(...this.list.map(x => x.getLines()));
     }
 }
 
@@ -63,6 +66,38 @@ class Sequence {
     getHeight() {
         return this.list.map(x => x.getHeight()).reduce((a, b) => a + b, 0);
     }
+    getLines() {
+        let res = [];
+        let last = null;
+        for (const x of this.list) {
+            if (!last) {
+                last = x;
+                continue;
+            }
+
+            if (last instanceof Node) {
+                if (x instanceof Node) {
+                    res.push({from: last.id, to: [x.id]});
+                } else {
+                    //Choice
+                    res.push({
+                        from: last.id,
+                        to: x.getEntering().map(x => x.id)
+                    });
+                }
+            } else {
+                // last = Choice, x = Node
+                res.push({from: x.id, to: last.getLeaving().map(x => x.id)});
+            }
+
+            if (x instanceof Choice) {
+                res = res.concat(x.getLines());
+            }
+
+            last = x;
+        }
+        return res;
+    }
 }
 
 const testTree = new Sequence()
@@ -89,7 +124,7 @@ class HoriList extends React.Component {
         return (
             <div>
                 {this.props.choice.list.map(x => (
-                    <List sequ={x} />
+                    <List key={x.id} sequ={x} />
                 ))}
                 <style jsx>{`
                     div {
@@ -107,66 +142,25 @@ class List extends React.Component {
         super(props);
     }
     render() {
-        const hori = this.horizontal;
-        const lines = [];
         const children = [];
-        let last = null;
 
         for (const x of this.props.sequ.list) {
             if (x instanceof Choice) {
-                if (last) {
-                    for (const e of x.getEntering()) {
-                        lines.push(
-                            <SteppedLineTo
-                                from={`square-${last.id}`}
-                                to={`square-${e.id}`}
-                            />
-                        );
-                    }
-                }
-                children.push(<HoriList choice={x} />);
+                children.push(<HoriList key={x.id} choice={x} />);
             } else {
-                if (last) {
-                    if (last instanceof Choice) {
-                        for (const e of last.getLeaving()) {
-                            lines.push(
-                                <SteppedLineTo
-                                    from={`square-${x.id}`}
-                                    to={`square-${e.id}`}
-                                />
-                            );
-                        }
-                    } else {
-                        lines.push(
-                            <SteppedLineTo
-                                from={`square-${last.id}`}
-                                to={`square-${x.id}`}
-                            />
-                        );
-                    }
-                }
                 children.push(<Square key={x.id} info={x} />);
             }
-            last = x;
         }
 
-        // TODO: react-lineto doesn't work on server rendered applications.
-        //       process.browser is a workaround, create own component
-        //       that uses componentDidMount or something
         return (
             <div className="list-container">
                 {children}
-                {process.browser ? lines : null}
-                <div className="anchor" />
+                {/* <div className="anchor" /> */}
                 <style jsx>
                     {`
                         .list-container {
                             display: flex;
                             flex-direction: column;
-                        }
-                        .anchor {
-                            margin-top: auto;
-                            width: 100%;
                         }
                     `}
                 </style>
@@ -178,7 +172,7 @@ class List extends React.Component {
 class Square extends React.Component {
     render() {
         return (
-            <div className={`square-${this.props.info.id}`}>
+            <div className={`square square-${this.props.info.id}`}>
                 {this.props.info.text}
                 <style jsx>{`
                     div {
@@ -193,8 +187,138 @@ class Square extends React.Component {
     }
 }
 
+// TODO: handles updates??
+class Lines extends React.Component {
+    constructor(props) {
+        super(props);
+        this.stageWidth = null;
+        this.stageHeight = null;
+        this.stageTop = null;
+        this.stageLeft = null;
+        this.squareSizes = {};
+        this.stage_wrapper = React.createRef();
+    }
+    componentDidMount() {
+        this.updateDOMSizes();
+    }
+    updateDOMSizes() {
+        let {
+            top,
+            left,
+            width,
+            height
+        } = this.stage_wrapper.current.getBoundingClientRect();
+        this.stageWidth = width;
+        this.stageHeight = height;
+        this.stageTop = top;
+        this.stageLeft = left;
+
+        const reg = /square-(\d+)/;
+        this.squareSizes = Array.prototype.map
+            .call(document.getElementsByClassName("square"), ele => ({
+                id: Array.prototype.find
+                    .call(ele.classList, c => reg.test(c))
+                    .match(reg)[1],
+                rect: ele.getBoundingClientRect()
+            }))
+            .reduce(
+                (base, {id, rect}) =>
+                    Object.assign(base, {
+                        [id]: {
+                            x: rect.left + rect.width / 2,
+                            y: rect.top + rect.height / 2,
+                            width: rect.width,
+                            height: rect.height,
+                            left: rect.left,
+                            right: rect.right,
+                            bottom: rect.bottom,
+                            top: rect.top
+                        }
+                    }),
+                {}
+            );
+
+        this.forceUpdate();
+    }
+    renderLines() {
+        let res = [];
+        for (const l of this.props.lines) {
+            const from = this.squareSizes[l.from];
+            const to = l.to.map(x => this.squareSizes[x]);
+
+            let commonMiddle = to.reduce((base, x) => {
+                const cand = (x.y - from.y) / 2;
+                return Math.abs(cand) < Math.abs(base) ? cand : base;
+            }, Infinity);
+
+            const stem = [from.x, from.y, from.x, from.y + commonMiddle];
+
+            for (const t of to) {
+                const points = stem
+                    .concat([t.x, from.y + commonMiddle, t.x, t.y])
+                    .map((x, i) =>
+                        i % 2 === 0 ? x + this.stageLeft : x + this.stageTop
+                    );
+                res.push(
+                    <Line
+                        key={points.join(" ")}
+                        points={points}
+                        stroke={"green"}
+                        strokeWidth={2}
+                    />
+                );
+            }
+        }
+        return res;
+    }
+    render() {
+        return (
+            <div className="stage-wrapper" ref={this.stage_wrapper}>
+                {this.stageWidth && this.stageHeight && (
+                    <Stage width={this.stageWidth} height={this.stageHeight}>
+                        <Layer>
+                            {Object.keys(this.squareSizes).length !== 0 &&
+                                this.props.lines.length !== 0 &&
+                                this.renderLines()}
+                        </Layer>
+                    </Stage>
+                )}
+                <style jsx>
+                    {`
+                        .stage-wrapper {
+                            width: 100%;
+                            height: 100%;
+                            position: absolute;
+                            top: 0px;
+                            left: 0px;
+                            z-index: -1;
+                        }
+                    `}
+                </style>
+            </div>
+        );
+    }
+}
+
+class Tree extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tree: testTree
+        };
+    }
+    render() {
+        return (
+            <div className="tree-root">
+                <List sequ={this.state.tree} />
+                <Lines lines={this.state.tree.getLines()} />
+            </div>
+        );
+    }
+}
+
 function Page(props) {
-    return <List sequ={testTree} />;
+    return <Tree />;
 }
 
 export default Page;
